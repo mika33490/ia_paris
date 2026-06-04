cd7322bf48bf911c33052f33b3fc21a69ff57fb1
mika33490 <mickael.ithie@gmail.com>
2026-06-04 22:20:16 +0200

    ajout tennis boucle

--- run_loop.py
+++ run_loop.py
@@ -2,12 +2,19 @@ import time
 import subprocess
 
 while True:
-    print("🚀 Lancement analyse IA...")
+    print("🚀 Lancement analyse FOOT...")
 
     try:
         subprocess.run(["python", "main.py"], check=False)
     except Exception as e:
-        print("ERREUR LOOP:", e)
+        print("ERREUR FOOT:", e)
+
+    print("🎾 Lancement analyse TENNIS...")
+
+    try:
+        subprocess.run(["python", "tennis.py"], check=False)
+    except Exception as e:
+        print("ERREUR TENNIS:", e)
 
     print("⏳ Prochaine analyse dans 3 heures...")
     time.sleep(3 * 60 * 60)
